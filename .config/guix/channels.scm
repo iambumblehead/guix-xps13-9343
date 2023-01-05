d233e318533ae2a5f3e24b244eb45b0c9326831f
chris <chris@bumblehead.com>
2023-01-05 11:41:26 -0800

    use pinned guix channel

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,18 +1,14 @@
-;; the 'guix channel definition is included with %default-channels
-;;
-;; ```scm
-;; (channel
-;;  (name 'guix)
-;;  (url "https://git.savannah.gnu.org/git/guix.git")
-;;  (commit "v1.4.0")
-;;  (introduction
-;;   (make-channel-introduction
-;;    "9edb3f66fd807b096b48283debdcddccfea34bad"
-;;    (openpgp-fingerprint
-;;     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-;; ```
-;;
+; guix and nonguix channels are pinned to compatible commit version
 (append (list
+         (channel
+          (name 'guix)
+          (url "https://git.savannah.gnu.org/git/guix.git")
+          (commit "v1.4.0")
+          (introduction
+           (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
          (channel
           (name 'nonguix)
           (url "https://gitlab.com/nonguix/nonguix")
